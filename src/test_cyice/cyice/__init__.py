a3c325c0c67aa0a35fb0ace845c198fcce85df95
Andrew Annex <2126916+AndrewAnnex@users.noreply.github.com>
2025-11-25 11:47:54 -0500

    restructure to avoid nested cyice folder

--- src/test_cyice/cyice/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from ..utils import ctypes_cspice
-except ImportError:  # fallback if executed outside package context
-    from test_cyice.utils import ctypes_cspice
-
-from .cyice import *
